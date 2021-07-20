cce94ff6807abd89d30a3d377f93e81fddeb071d
Sonam-1122 <sonamg2002@gmail.com>
2021-07-20 16:28:39 +0530

    resume design improved

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -119,182 +119,125 @@ function Resume() {
                 ) : null}
 
                 <div className="inner-container">
-                  {formData.skill.map((val, key) => console.log(key, val))}
-                  {/* {formData.extra.skill1Value ? (
-                    <div>
-                      <hr />
-                      <h4>Skills/languages</h4>
-                    </div>
-                  ) : null}
-                  {formData.extra.skill1Value ? (
-                    <p>
-                      <i className="fas fa-certificate"></i>{" "}
-                      {formData.extra.skill1Value}
-                    </p>
-                  ) : null} */}
-                </div>
-                <div className="inner-container">
-                  <h4>Interests</h4>
-                  {formData.interest.map((val, key) => (
+                  <h5>Skills/languages</h5>
+                  {formData.skill.map((val, key) => (
                     <div key={key}>
-                      <hr className="mt-1 mb-1" />
-                      <i className="icon fas fa-user-plus"></i>
-                      {formData.interest[key].interestValue}
+                      {formData.skill[key].skillValue ? (
+                        <div>
+                          {formData.skill[key].skillValue}
+                          <hr className="mt-1 mb-1 w-50 mx-auto" />
+                        </div>
+                      ) : null}
                     </div>
                   ))}
-                  {/* {formData.extra.interest1Value ? (
-                    <div>
-                      <hr />
-                      <h4>Interests</h4>
+                </div>
+                <div className="inner-container mt-5">
+                  <h5>Interests</h5>
+                  {formData.interest.map((val, key) => (
+                    <div key={key}>
+                      {formData.interest[key].interestValue ? (
+                        <div>
+                          {formData.interest[key].interestValue}
+                          <hr className="mt-1 mb-1 w-50 mx-auto" />
+                        </div>
+                      ) : null}
                     </div>
-                  ) : null}
-                  {formData.extra.interest1Value ? (
-                    <p>
-                      <i className="fas fa-user-plus"></i>{" "}
-                      {formData.extra.interest1Value}
-                    </p>
-                  ) : null} */}
+                  ))}
                 </div>
               </div>
             </div>
             <div className="col-md-8 py-5">
-              <h1 id="nameT2" className="text-center">
+              <h3 id="nameT2" className="text-center">
                 <i className="icon fas fa-id-card-alt"></i>{" "}
                 {formData.personal.firstNameValue}{" "}
                 {formData.personal.lastNameValue}
-              </h1>
-              {formData.educational.schoolNameValue ? (
-                <div className="card mt-4">
-                  <div className="card-header background">
-                    <h3>Educational</h3>
-                  </div>
-                  <div className="card-body">
-                    <h4>
-                      <i className="icon fas fa-graduation-cap"></i> School Info
-                    </h4>
-                    <h6 className="d-inline">School Name : </h6>
-                    {formData.educational.schoolNameValue}
-                    <br />
-                    <h6 className="d-inline">From : </h6>
-                    {formData.educational.schoolFromDateValue}
-                    <br />
-                    <h6 className="d-inline"> To : </h6>
-                    {formData.educational.schoolToDateValue}
-                    <br />
-                    <h6 className="d-inline"> Qualification : </h6>
-                    {formData.educational.schoolQualificationValue}
-                    <br />
-                    <h6 className="d-inline"> Description : </h6>
-                    {formData.educational.schoolDescriptionValue}
-                    <hr />
-                    <h4>
-                      <i className="icon fas fa-graduation-cap"></i> College
-                      Info
-                    </h4>
-                    <h6 className="d-inline">School Name : </h6>
-                    {formData.educational.clgNameValue}
-                    <br />
-                    <h6 className="d-inline">From : </h6>
-                    {formData.educational.clgFromDateValue}
-                    <br />
-                    <h6 className="d-inline"> To : </h6>
-                    {formData.educational.clgToDateValue}
-                    <br />
-                    <h6 className="d-inline"> Qualification : </h6>
-                    {formData.educational.clgQualificationValue}
-                    <br />
-                    <h6 className="d-inline"> Description : </h6>
-                    {formData.educational.clgDescriptionValue}
-                  </div>
-                </div>
-              ) : null}
+              </h3>
 
-              {formData.projects.pro1TitleValue ? (
-                <div className="card mt-4">
-                  <div className="card-header background">
-                    <h3>Projects</h3>
-                  </div>
-                  <div className="card-body">
-                    <h4>
-                      <i className="fas fa-tasks"></i> Project 1
-                    </h4>
-                    <h6 className="d-inline"> Title : </h6>
-                    {formData.projects.pro1TitleValue}
-                    <br />
-                    <h6 className="d-inline"> Link : </h6>
-                    {formData.projects.pro1LinkValue}
-                    <br />
-                    <h6 className="d-inline"> Description : </h6>
-                    {formData.projects.pro1DescriptionValue}
-                    {formData.projects.pro2TitleValue ? (
-                      <div>
-                        <hr />
-                        <h4>
-                          <i className="fas fa-tasks"></i> Project 2
-                        </h4>
-                        <h6 className="d-inline"> Title : </h6>
-                        {formData.projects.pro2TitleValue}
-                        <br />
-                        <h6 className="d-inline"> Link : </h6>
-                        {formData.projects.pro2LinkValue}
-                        <br />
-                        <h6 className="d-inline"> Description : </h6>
-                        {formData.projects.pro2DescriptionValue}
-                      </div>
-                    ) : null}
-                    {formData.projects.pro3TitleValue ? (
-                      <div>
-                        <hr />
-                        <h4>
-                          <i className="fas fa-tasks"></i> Project 3
-                        </h4>
-                        <h6 className="d-inline"> Title : </h6>
-                        {formData.projects.pro3TitleValue}
-                        <br />
-                        <h6 className="d-inline"> Link : </h6>
-                        {formData.projects.pro3LinkValue}
-                        <br />
-                        <h6 className="d-inline"> Description : </h6>
-                        {formData.projects.pro3DescriptionValue}
-                      </div>
-                    ) : null}
-                  </div>
+              <div className="card mt-4">
+                <div className="card-header background">
+                  <h4>
+                    <i className="icon fas fa-graduation-cap"></i> Educational
+                    Info
+                  </h4>
                 </div>
-              ) : null}
-              {formData.experience.exp1InstituteValue ? (
-                <div className="card mt-4">
-                  <div className="card-header background">
-                    <h3>Experience</h3>
-                  </div>
-                  <div className="card-body">
-                    <h5>
-                      <i className="icon fas fa-user-tie"></i>{" "}
-                      {formData.experience.exp1InstituteValue} -{" "}
-                      {formData.experience.exp1PositionValue}
-                    </h5>
-                    <h6 className="d-inline">Duration : </h6>
-                    {formData.experience.exp1DurationValue}
-                    <br />
-                    <h6 className="d-inline">Description : </h6>
-                    {formData.experience.exp1DescriptionValue}
-                    {formData.experience.exp2InstituteValue ? (
-                      <div>
-                        <hr />
-                        <h5>
-                          <i className="icon fas fa-user-tie"></i>{" "}
-                          {formData.experience.exp2InstituteValue} -{" "}
-                          {formData.experience.exp2PositionValue}
-                        </h5>
-                        <h6 className="d-inline">Duration : </h6>
-                        {formData.experience.exp2DurationValue}
-                        <br />
-                        <h6 className="d-inline">Description : </h6>
-                        {formData.experience.exp2DescriptionValue}
-                      </div>
-                    ) : null}
-                  </div>
+                <div className="card-body">
+                  {formData.educational.map((val, key) => (
+                    <div key={key}>
+                      {formData.educational[key].schoolNameValue ? (
+                        <div>
+                          <h5>
+                            {formData.educational[key].schoolQualificationValue}
+                          </h5>
+                          <h6 className="d-inline">School Name : </h6>
+                          {formData.educational[key].schoolNameValue}
+                          <br />
+                          <h6 className="d-inline">From : </h6>
+                          {formData.educational[key].schoolFromDateValue}
+                          <br />
+                          <h6 className="d-inline"> To : </h6>
+                          {formData.educational[key].schoolToDateValue}
+                          <br />
+                          <h6 className="d-inline"> Description : </h6>
+                          {formData.educational[key].schoolDescriptionValue}
+                          <hr />
+                        </div>
+                      ) : null}
+                    </div>
+                  ))}
                 </div>
-              ) : null}
+              </div>
+
+              <div className="card mt-4">
+                <div className="card-header background">
+                  <h4>
+                    <i className="fas fa-tasks"></i> Projects
+                  </h4>
+                </div>
+                <div className="card-body">
+                  {formData.projects.map((val, key) => (
+                    <div key={key}>
+                      {formData.projects[key].proTitleValue ? (
+                        <div>
+                          <h5>{formData.projects[key].proTitleValue}</h5>
+                          <h6 className="d-inline"> Link : </h6>
+                          {formData.projects[key].proLinkValue}
+                          <br />
+                          <h6 className="d-inline"> Description : </h6>
+                          {formData.projects[key].proDescriptionValue}
+                          <hr />
+                        </div>
+                      ) : null}
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className="card mt-4">
+                <div className="card-header background">
+                  <h4>
+                    <i className="icon fas fa-user-tie"></i> Experience
+                  </h4>
+                </div>
+                <div className="card-body">
+                  {formData.experience.map((val, key) => (
+                    <div key={key}>
+                      {formData.experience[key].expInstituteValue ? (
+                        <div>
+                          <h5>
+                            {formData.experience[key].expInstituteValue} -{" "}
+                            {formData.experience[key].expPositionValue}
+                          </h5>
+                          <h6 className="d-inline">Duration : </h6>
+                          {formData.experience[key].expDurationValue}
+                          <br />
+                          <h6 className="d-inline">Description : </h6>
+                          {formData.experience[key].expDescriptionValue}
+                          <hr />
+                        </div>
+                      ) : null}
+                    </div>
+                  ))}
+                </div>
+              </div>
             </div>
             <button
               onClick={(e) => printResume(e)}
