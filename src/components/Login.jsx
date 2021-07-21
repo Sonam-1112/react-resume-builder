ea7d1e7405a89ee859efe2f61e779928eb79128b
Sonam-1122 <sonamg2002@gmail.com>
2021-07-21 10:30:55 +0530

    login page changed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -33,38 +33,45 @@ function Login() {
       });
   }
   return (
-    <div className="base-container">
-      <div className="header">Login</div>
-      {error}
-      <div className="content">
-        <div className="image">
-          <img src={login_img} alt="" />
-        </div>
-        <form className="entry-form" onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Email</label>
-            <input
-              type="email"
-              name="email"
-              placeholder="email"
-              ref={emailRef}
-            />
+    <div className="container bg-info text-dark text-center mt-0 p-5 h-25 mt-3">
+      <h2>Login</h2>
+      <h5 className="text-danger">{error}</h5>
+      <div className="content row">
+        <div className="px-5">
+          <img src={login_img} alt="" className="img-fluid w-50" />
+          <form className="entry-form" onSubmit={handleSubmit}>
+            <div className="form-group">
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                className="form-control w-75 mx-auto mt-4"
+                ref={emailRef}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                className="form-control  w-75 mx-auto mt-4"
+                ref={passwordRef}
+              />
+            </div>
+            <button
+              type="submit"
+              className="btn btn-success mt-4 w-25 text-center"
+            >
+              Login
+            </button>
+          </form>
+          <div className="switch-link mt-3">
+            <h6 className="d-inline">Not have an account ? </h6>
+            <Link className="btn btn-sm btn-secondary" to="/register">
+              {" "}
+              Register Now
+            </Link>
           </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              placeholder="password"
-              ref={passwordRef}
-            />
-          </div>
-          <button type="submit" className="btn">
-            Login
-          </button>
-        </form>
-        <div className="switch-link">
-          Not have an account ?<Link to="/register"> Register Now</Link>
         </div>
       </div>
     </div>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -37,47 +37,51 @@ function Register() {
       });
   }
   return (
-    <div className="base-container">
-      <div className="header">Register</div>
-      {error}
-      <div className="content">
-        <div className="image">
-          <img src={login_img} alt="" />
-        </div>
-        <form className="entry-form" onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Email</label>
-            <input
-              type="email"
-              name="email"
-              placeholder="email"
-              ref={emailRef}
-            />
+    <div className="container bg-info text-dark text-center mt-3 p-4">
+      <h2>Register</h2>
+      <h5 className="text-danger">{error}</h5>
+      <div className="content row">
+        <div className="px-5">
+          <img src={login_img} alt="" className="img-fluid w-50" />
+          <form className="entry-form" onSubmit={handleSubmit}>
+            <div className="form-group">
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                className="form-control w-75 mx-auto mt-4"
+                ref={emailRef}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                className="form-control w-75 mx-auto mt-4"
+                ref={passwordRef}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                name="conpassword"
+                placeholder="Confirm Password"
+                className="form-control w-75 mx-auto mt-4"
+                ref={conpasswordRef}
+              />
+            </div>
+            <button type="submit" className="btn btn-success mt-4 w-25">
+              Register
+            </button>
+          </form>
+          <div className="switch-link mt-3">
+            <h6 className="d-inline">Already have an account ? </h6>
+            <Link className="btn btn-sm btn-secondary" to="/">
+              {" "}
+              Sign in
+            </Link>
           </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              placeholder="password"
-              ref={passwordRef}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="conpassword">Confirm Password</label>
-            <input
-              type="password"
-              name="conpassword"
-              placeholder="Confirm Password"
-              ref={conpasswordRef}
-            />
-          </div>
-          <button type="submit" className="btn">
-            Register
-          </button>
-        </form>
-        <div className="switch-link">
-          Already have an account ?<Link to="/"> Sign in</Link>
         </div>
       </div>
     </div>
